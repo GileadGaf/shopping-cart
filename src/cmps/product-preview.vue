fbe153a6d6699071102871be1deb60e2b18337e3
Guy Geva <guy1030@gmail.com>
2021-08-01 14:10:34 +0300

    Guy-Creating cmps

---
+++ src/cmps/product-preview.vue
@@ -0,0 +1,25 @@
+<template>
+	<section class="product-preview">
+		<img :src="product.img" class="product-img" />
+		<p class="product-price">{{ product.price }}</p>
+		<p class="product-title">{{ title }}</p>
+		<button @click.stop="addToCart" class="product-add-to-cart">ADD TO CART</button>
+	</section>
+</template>
+
+<script>
+export default {
+	props: {
+		products: {
+			type: Object
+		},
+	},
+  methods: {
+    addToCart() {
+      console.log('addToCart');
+      this.$store.commit({type:'addToCart', product})
+    }
+  },
+
+}
+</script>
---
+++ src/pages/products.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="products">
+    <product-list />
+  </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
