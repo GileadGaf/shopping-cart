91f55d6e1da64ef158d7dcfa89156f062d5dd9c8
Gilead <gileadgafni4s@gmail.com>
2021-08-01 12:56:29 +0300

    Initializing CLI

---
+++ src/cmps/app-header.vue
@@ -0,0 +1,22 @@
+<template>
+  <header>
+      <div class="logo">
+         <h1> Shopping</h1>
+      </div>
+       <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> |
+      <router-link to="/cart">My cart</router-link>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
