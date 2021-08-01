d2cf7197c3deb5fdd0e3e0e9495bd479b6175a7f
Gilead <gileadgafni4s@gmail.com>
2021-08-01 14:09:54 +0300

    started store

---
+++ src/store/modules/product-store.js
@@ -0,0 +1,35 @@
+import { productService } from '../../services/product-service';
+
+export default {
+    state: {
+        cartProducts: []
+    },
+    getters: {
+        cartProducts({ cartProducts }) {
+            return cartProducts;
+        },
+        cartSize({ cartProducts }) {
+            return cartProducts.length;
+        },
+        cartTotal({ cartProducts }) {
+            return cartProducts.reduce((acc, product) => {
+                return acc + product.price;
+            }, 0)
+        }
+    },
+    mutations: {
+        addToCart({ cartProducts }, { product }) {
+            cartProducts.push(product);
+        },
+        removeFromCart({ cartProducts }, { productId }) {
+            const idx = state.cartProducts.findIndex(product => cproduct_id === productId)
+            cartProducts.splice(idx, 1)
+        },
+        clearCart({ cartProducts }) {
+            cartProducts = [];
+        }
+
+    },
+    actions: {},
+    modules: {}
+}
